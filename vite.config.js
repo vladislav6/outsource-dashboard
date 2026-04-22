152f650a04fc154b3b568f8297c64ac6ce8908b0
Vladislav <vlad.kvan96@gmail.com>
2026-04-22 23:28:20 +0300

    init: start dashboard project

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: './',
+  build: {
+    minify: false,
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: 'index.html'
+      }
+    }
+  }
+});
